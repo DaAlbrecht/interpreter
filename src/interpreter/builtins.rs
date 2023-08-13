d710eb41eedaa464ee61c829a9a0ce5963f6942d
DaAlbrecht <dadidi42@gmail.com>
2023-08-13 17:31:36 +0200

    feat: 4.3

--- src/interpreter/builtins.rs
+++ src/interpreter/builtins.rs
@@ -9,23 +9,17 @@ pub fn get_builtins() -> HashMap<String, Object> {
 }
 
 pub fn len(args: Option<Vec<Object>>) -> Object {
-    if let Some(args) = args {
-        if args.len() != 1 {
-            return Object::Error(format!(
+    match args {
+        Some(args) => match args.len() {
+            1 => match &args[0] {
+                Object::String(s) => Object::Int(s.len() as i64),
+                _ => Object::Error(format!("argument to `len` not supported, got {}", args[0])),
+            },
+            _ => Object::Error(format!(
                 "wrong number of arguments. got={}, want=1",
                 args.len()
-            ));
-        } else {
-            match &args[0] {
-                Object::String(s) => return Object::Int(s.len() as i64),
-                _ => {
-                    return Object::Error(format!(
-                        "argument to `len` not supported, got {}",
-                        args[0]
-                    ))
-                }
-            }
-        }
+            )),
+        },
+        None => Object::Error("wrong number of arguments. got=0, want=1".to_string()),
     }
-    Object::Error("wrong number of arguments. got=0, want=1".to_string())
 }
